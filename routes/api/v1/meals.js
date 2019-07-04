8a3870acd69edb8accde93c79405a27b7e7e0209
Ethan Grab <ethangrab@gmail.com>
2019-07-04 17:38:57 -0600

    all routes for meals

--- routes/api/v1/meals.js
+++ routes/api/v1/meals.js
@@ -10,56 +10,64 @@ const Meal = require('../../../models/meal');
 
 
 router.get('/', async (request, response) => {
-  // var pry = require('pryjs');
-  // eval(pry.it);
   try {
-    await Meal.query()
-        .findById(1)
-      }
-      catch (error) {
-        response.status(404).json({ error })
-      }
-
-  // database('meals').select()
-  //   .then(meals => {
-  //     return response.status(200).json(meals);
-  //   })
-  //   .catch((error) => {
-  //     return response.status(500).json({ error });
-  //   });
-
+    const meals = await Meal
+      .query()
+      .eager('foods');
+    response.send(meals)
+    }
+    catch (error) {
+      response.status(404).json({ error })
+    }
 });
-
-
 
 router.post('/:meal_id/foods/:id', async (request, response) => {
-  var pry = require('pryjs');
-  const food = await Food.query().findById(request.params.id);
-  eval(pry.it);
+  try
+    {
+    // Find specific food
+    const food = await Food.query().findById(request.params.id)
+    const meal = await Meal.query().findById(request.params.meal_id)
+    // Connect food to specified meal
+    const numRelatedRows = await food.$relatedQuery('meals').relate(meal.id);
 
-  database('meals').insert(hedgehog, 'id')
-    .then((hedgehogId) => {
-      return response.status(201).json({ id: hedgehogId[0] });
-    })
-    .catch((error) => {
-      return response.status(500).json({ error });
-    });
+    response.send( 'message: Successfully added ' + food.name + ' to ' + meal.name )
+    }
+    catch (error)
+      {
+        response.status(404).json({ error })
+      }
 });
 
-// router.post('/', async (request, response) => {
-//   var pry = require('pryjs');
-//   eval(pry.it);
-//
-//   const this = await Meal.query()
-//           .insert({ name: 'TestMeal' })
-//           .then(meal) => {
-//             return response.status(201).json({ id: meal[0] });
-//           }
-//           .catch((error) => {
-//             return response.status(500).json({ error });
-//           });
-// });
+router.get('/:meal_id/foods', async (request, response) => {
+  try
+    {
+    const meal = await Meal
+      .query()
+      .findById(request.params.meal_id)
+      .eager('foods');
 
+    response.send(meal)
+    }
+    catch (error)
+      {
+        response.status(404).json({ error })
+      }
+});
 
+router.delete('/:meal_id/foods/:id', async (request, response) => {
+  try
+    {
+      const meal = await Meal.query().findById(request.params.meal_id)
+      await meal
+      .$relatedQuery('foods')
+      .unrelate()
+      .where('id', request.params.id)
+      response.status(204)
+    }
+      catch (error)
+        {
+          response.status(404).json({ error })
+        }
+});
 
 module.exports = router;
