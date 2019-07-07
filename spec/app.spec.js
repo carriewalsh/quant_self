97e5dc8e6b2506cbde32a5cfce3ec8ed08b921f7
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-07-07 13:33:15 -0600

    Passes test for api/v1/foods 200 code

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -4,7 +4,7 @@ var app = require('../app');
 
 describe('Test the root path', () => {
   test('It should respond to the GET method', () => {
-    return request(app).get("/").then(response => {
+    return request(app).get("/api/v1/foods").then(response => {
       expect(response.statusCode).toBe(200)
     })
   });
