5d1354787366df6575995806c79738de52adcf1b
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-07-07 15:44:46 -0600

    Adds packages for rendering pages (consolidate, mustache, path, express-cors)

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const environment = process.env.NODE_ENV || "development";
 const configuration = require("./knexfile")[environment];
 const database = require("knex")(configuration);
 
+const cors = require('express-cors');
+const path = require('path');
+
 
 var indexRouter = require('./routes/index');
 var foodsRouter = require('./routes/api/v1/foods');
@@ -15,15 +18,28 @@ app.use('/', indexRouter);
 app.use('/api/v1/foods', foodsRouter);
 app.use('/api/v1/meals', mealsRouter);
 
+// app.engine('html')
+app.use(express.static(path.join(__dirname + '/public')));
 
-app.get('/api/v1/foods', (req,res) => {
-  database('foods').select()
-    .then(data => {
-      console.log(data)
-    })
-    .catch(error => {
-      console.log(error)
-    })
-})
+app.use(cors());
+app.set('views', path.join(__dirname, 'views'));
+
+var engines = require('consolidate');
+
+app.set('views', __dirname + '/views');
+app.engine('html', engines.mustache);
+app.set('view engine', 'html');
+
+
+// app.get('/api/v1/foods', (req,res) => {
+//   database('foods').select()
+//     .then(data => {
+//       console.log(data)
+//     })
+//     .catch(error => {
+//       console.log(error)
+//     })
+// })
+
 
 module.exports = app;
