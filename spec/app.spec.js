71fd538f85909e5e6c134f205d38a39a21645318
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-07-07 14:26:42 -0600

    Adds test for delete - not working maybe because of relationships

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -23,7 +23,7 @@ describe('api', () => {
   test('It should list the foods', () => {
     return request(app).get("/api/v1/foods").then(response => {
       expect(response.body.length).toEqual(8)
-      expect(response.body[0]["name"]).toContain("cheese")
+      expect(response.body[0]["name"]).toEqual("cheese")
     })
   })
 
@@ -38,6 +38,7 @@ describe('api', () => {
   test('It should add a new food', () => {
     const newFood = {name: "test_food", calories: 0}
     return request(app).post("/api/v1/foods").send(newFood).then(response => {
+      expect(response.statusCode).toBe(200)
       expect(response.body["message"]).toEqual("test_food has been added to your foods")
       expect(response.body["data"]["name"]).toEqual("test_food")
     })
@@ -46,9 +47,22 @@ describe('api', () => {
   test('It should edit a food', () => {
     const editFood = {name: "cheddar cheese"}
     return request(app).patch('/api/v1/foods/1').send(editFood).then(response => {
+      expect(response.statusCode).toBe(200)
       expect(response.body["message"]).toEqual("cheddar cheese has been edited")
       expect(response.body["data"]["id"]).toEqual(1)
       expect(response.body["data"]["name"]).not.toEqual("cheese")
     })
+  })
+
+  xtest('It should delete a food', () => {
+    return request(app).delete('/api/v1/foods/1').then(response => {
+      // expect(response.statusCode).toBe(200)
+      console.log(response.error)
+      expect(response.body["message"]).toEqual("cheese has been deleted.")
+    })
+    // return request(app).get("/api/v1/foods").then(response => {
+    //   expect(response.body.length).toEqual(7)
+    //   expect(response.body[0]["name"]).not.toEqual("cheese")
+    // })
   })
 });
