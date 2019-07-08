9f52a7b6a8a18f77fd5528e0139fc766ca1c746b
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-07-07 18:14:40 -0600

    Adds my_meals view and get request

--- routes/users.js
+++ routes/users.js
@@ -4,11 +4,28 @@ const environment = process.env.NODE_ENV || 'development';
 const configuration = require('../knexfile')[environment];
 const database = require('knex')(configuration);
 var Food = require('../models/food.js')
+var Meal = require('../models/meal.js')
 
 /* GET home page. */
 router.get('/my_foods', async function(req, res, next) {
-  foods = await Food.query();
-  console.log(foods)
-  res.render('my_foods.ejs', {foods: foods});
+  try {
+    const foods = await Food.query();
+    res.render('my_foods.ejs', {foods: foods});
+  }
+  catch (error) {
+    res.status(404).json({error})
+  }
+})
 
+router.get('/my_meals', async function(req, res, next) {
+  console.log(req)
+  try {
+    const meals = await Meal
+      .query().eager('foods');
+      res.render('my_meals.ejs', {meals: meals});
+    }
+  catch (error) {
+    res.status(404).json({ error })
+  }
+})
 module.exports = router;
