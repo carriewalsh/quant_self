46feaf482c9ab29f4a906bf33e9df0c411fe3df2
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-07-09 16:38:49 -0600

    REARRANGES migrations and seeds - need to drop both databases and recreate and remigrate - to add users

--- db/migrations/20190701204240_initial.js
+++ db/migrations/20190701204240_initial.js
@@ -1,6 +1,15 @@
 
 exports.up = function(knex) {
   return Promise.all([
+    knex.schema.createTable('users', function(table) {
+      table.increments('id').primary();
+      table.string('name');
+      table.string('email');
+      table.string('password');
+      table.string('apiKey')
+      table.timestamps(true,true);
+    }),
+
     knex.schema.createTable('foods', function(table) {
       table.increments('id').primary();
       table.string('name');
@@ -10,12 +19,17 @@ exports.up = function(knex) {
     knex.schema.createTable('meals', function(table) {
       table.increments('id').primary();
       table.string('name');
+      table.integer('user_id').unsigned();
+      table.foreign('user_id').references('users.id')
+      // table.integer('user_id').references('id').inTable('users').onDelete('CASCADE')
       table.timestamps(true,true);
     }),
     knex.schema.createTable('food_meals', function(table) {
       table.increments('id').primary();
-      table.integer('food_id').unsigned().index().references('id').inTable('foods');
-      table.integer('meal_id').unsigned().index().references('id').inTable('meals');
+      table.integer('food_id').unsigned()
+      table.foreign('food_id').references('foods.id')
+      table.integer('meal_id').unsigned()
+      table.foreign('meal_id').references('meals.id')
     })
   ])
 };
@@ -24,6 +38,7 @@ exports.down = function(knex) {
   return Promise.all([
     knex.schema.dropTable('food_meals'),
     knex.schema.dropTable('foods'),
-    knex.schema.dropTable('meals')
+    knex.schema.dropTable('meals'),
+    knex.schema.dropTable('users')
   ])
 };
