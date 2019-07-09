0e6d4a98a52e7a00f60492b6d35bdb82f8b4e4ae
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-07-09 08:00:12 -0600

    Changes my_foods route to use fetch instead of call database - works

--- routes/users.js
+++ routes/users.js
@@ -1,28 +1,69 @@
 var express = require('express');
 var router = express.Router();
+var fetch = require('node-fetch')
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('../knexfile')[environment];
 const database = require('knex')(configuration);
 var Food = require('../models/food.js')
 var Meal = require('../models/meal.js')
+const pry = require('pryjs')
 
-/* GET home page. */
-router.get('/my_foods', async function(req, res, next) {
+
+// router.get('/my_foods', async function(req, res, next) {
+//   try {
+//     const foods = await Food.query();
+//     res.render('my_foods.ejs', {foods: foods});
+//   }
+//   catch (error) {
+//     res.status(404).json({error})
+//   }
+// })
+
+router.get('/my_foods', (req,res) => {
+  fetch('http://localhost:3000/api/v1/foods')
+    .then(response => {
+      if (response.ok) {
+        return response.json();}
+        throw new Error('Request Failed.');},
+        networkError => console.log(networkError.message))
+    .then(foods => {
+      res.render('my_foods.ejs', {foods: foods})
+    })
+    .catch((error) => {
+        console.log(error)
+      })
+  })
+
+router.get('/my_meals', async function(req, res) {
   try {
-    const foods = await Food.query();
-    res.render('my_foods.ejs', {foods: foods});
-  }
+    let meals = await Meal
+      .query().eager('foods');
+    var totalCalories = {}
+    meals.forEach(async meal => {
+      totalCalories[meal.id] = await meal.totalCalories()
+    })
+
+    res.render('my_meals.ejs', {
+      meals: meals,
+      totalCalories: totalCalories
+    });
+    }
   catch (error) {
-    res.status(404).json({error})
+    res.status(404).json({ error })
   }
 })
 
-router.get('/my_meals', async function(req, res, next) {
+router.get('/my_meals/:id', async function(req,res) {
   try {
-    const meals = await Meal
-      .query().eager('foods');
-      res.render('my_meals.ejs', {meals: meals});
-    }
+    let meal = await Meal.query().findById(req.params.id).eager('foods');
+    var totalCalories = await meal.totalCalories()
+    // eval(pry.it)
+    res.render('my_meals_id.ejs', {
+      meal: meal,
+      totalCalories: totalCalories,
+      foods: meal["foods"]
+    })
+  }
   catch (error) {
     res.status(404).json({ error })
   }
