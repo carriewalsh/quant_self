c21803701a8aa6050de4b5febcc3a21f7a214a96
Ethan Grab <ethangrab@gmail.com>
2019-07-09 10:42:26 -0600

    added food_meals route with get, post, delete and removed said routes from meals routes

--- routes/api/v1/food_meals.js
+++ routes/api/v1/food_meals.js
@@ -1,17 +1,28 @@
 var app = require('../../../app');
 var express = require('express');
-var router = express.Router();
+var router = express.Router( { mergeParams: true } );
 const environment = process.env.NODE_ENV || "development";
 const configuration = require("../../../knexfile")[environment];
 const database = require("knex")(configuration);
 const Food = require('../../../models/food');
 const Meal = require('../../../models/meal');
 const pry = require('pryjs')
-var mealsController = require('../../../controllers/api/v1/foods_controller')
+var foodMealsController = require('../../../controllers/api/v1/food_meals_controller')
 
+// router.param('id', function(req, res, next, id) {
+//   req.mealId = id;
+// });
 
 router.get('/', async (req,res) => {
-  mealsController.index(req,res)
+  foodMealsController.index(req,res)
+})
+
+router.post('/:id', async (req,res) => {
+  foodMealsController.create(req,res)
+})
+
+router.delete('/:id', async (req,res) => {
+  foodMealsController.destroy(req,res)
 })
 
 module.exports = router;
