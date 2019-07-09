0fd413aee1bf30dc523b325a4cc74bda023dffb2
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-07-09 12:59:35 -0600

    Adds jquery script to index and login

--- routes/users.js
+++ routes/users.js
@@ -84,42 +84,43 @@ router.get('/login', (req,res) => {
   res.render('login.ejs')
 })
 
-// router.post('/sessions', (req,res) => {
-//   var email = req.body.email
-//   var passwordAttempt = req.body.password
-//   if (validator.validate(email)) {
-//     User.findOne({where: {
-//       email: email
-//     }}).then(result => {
-//       if (result) {
-//         let passwordHash = result["dataValues"]["password"]
-//         let apiKey = result["dataValues"]["api_key"]
-//         let verify = bcrypt.compare(passwordAttempt, passwordHash)
-//         .then(comparison => {
-//           if (comparison) {
-//             session.addKey(apiKey) //will this work
-//           }
-//           else {
-//             res.setHeader("Content-Type", "application/json");
-//             res.status(404).json({
-//               error: "Email and password do not match."
-//             });
-//           }
-//         }).catch(error => {
-//             res.setHeader("Content-Type", "application/json");
-//             res.status(500).json({
-//               error: "Email does not exist in system."
-//             });
-//           });
-//       }
-//     })
-//   }
-//   else {
-//     res.setHeader("Content-Type", "application/json");
-//     res.status(409).json({
-//       error:`Invalid email address.`
-//     });
-// })
+router.post('/sessions', (req,res) => {
+  var email = req.body.email
+  var passwordAttempt = req.body.password
+  eval(pry.it)
+  if (validator.validate(email)) {
+    User.findOne({where: {
+      email: email
+    }}).then(result => {
+      if (result) {
+        let passwordHash = result["dataValues"]["password"]
+        let apiKey = result["dataValues"]["api_key"]
+        let verify = bcrypt.compare(passwordAttempt, passwordHash)
+        .then(comparison => {
+          if (comparison) {
+            session.addKey(apiKey) //will this work
+          }
+          else {
+            res.setHeader("Content-Type", "application/json");
+            res.status(404).json({
+              error: "Email and password do not match."
+            });
+          }
+        }).catch(error => {
+            res.setHeader("Content-Type", "application/json");
+            res.status(500).json({
+              error: "Email does not exist in system."
+            });
+          });
+      }
+    })
+  }
+  else {
+    res.setHeader("Content-Type", "application/json");
+    res.status(409).json({
+      error:`Invalid email address.`
+    });
+}})
 
 
 module.exports = router;
