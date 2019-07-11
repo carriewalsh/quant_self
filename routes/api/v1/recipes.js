0485f5f7708ef4f585bb2239a025330c255bed70
Ethan Grab <ethangrab@gmail.com>
2019-07-10 22:30:55 -0600

    Added routes for GET recipes/ calories_search & ingredient_search & health_search & diet_search & food_search which pulls on qe microservice

--- routes/api/v1/recipes.js
+++ routes/api/v1/recipes.js
@@ -6,27 +6,107 @@ const configuration = require("../../../knexfile")[environment];
 const database = require("knex")(configuration);
 const pry = require('pryjs')
 var request = require('request');
+const urlMicroService = 'http://localhost:3001/api/v1/recipes?'
+
+
+// function edamamRequest(error, response, body) {
+//     if (error) {
+//         console.log('error:', error); // Print the error if one occurred
+//
+//     } else if(response && body) {
+//         console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+//         // var json = JSON.stringify(body)
+//         res.send( JSON.parse(body) ); // Print JSON response.
+//     }
+// };
+
 
 router.get('/calories_search', async (req,res) => {
-  mealsController.index(req,res)
+  var queryObject = {
+      q: req.query.q,
+      calories: req.query.calories
+  }
+  request({
+      url: urlMicroService,
+      qs: queryObject
+  }, function (error, response, body) {
+      if (error) {
+          console.log('error:', error); // Print the error if one occurred
+
+      } else if(response && body) {
+          console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+          // var json = JSON.stringify(body)
+          res.send( JSON.parse(body) ); // Print JSON response.
+      }
+  })
 })
 
 router.get('/ingredient_search', async (req,res) => {
-  mealsController.index(req,res)
+  var queryObject = {
+      q: req.query.q,
+      ingr: req.query.ingr
+  }
+  request({
+      url: urlMicroService,
+      qs: queryObject
+  }, function (error, response, body) {
+      if (error) {
+          console.log('error:', error); // Print the error if one occurred
+
+      } else if(response && body) {
+          console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+          // var json = JSON.stringify(body)
+          res.send( JSON.parse(body) ); // Print JSON response.
+      }
+  })
 })
 
+router.get('/health_search', async (req,res) => {
+  var queryObject = {
+      q: req.query.q,
+      health: req.query.health
+  }
+  request({
+      url: urlMicroService,
+      qs: queryObject
+  }, function (error, response, body) {
+      if (error) {
+          console.log('error:', error); // Print the error if one occurred
+
+      } else if(response && body) {
+          console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+          // var json = JSON.stringify(body)
+          res.send( JSON.parse(body) ); // Print JSON response.
+      }
+  })
+})
+
+router.get('/diet_search', async (req,res) => {
+  var queryObject = {
+      q: req.query.q,
+      diet: req.query.diet
+  }
+  request({
+      url: urlMicroService,
+      qs: queryObject
+  }, function (error, response, body) {
+      if (error) {
+          console.log('error:', error); // Print the error if one occurred
+
+      } else if(response && body) {
+          console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+          // var json = JSON.stringify(body)
+          res.send( JSON.parse(body) ); // Print JSON response.
+      }
+  })
+})
 
 router.get('/food_search', function (req, res) {
-    var urlMicroService = 'http://localhost:3001/api/v1/recipes?'
     var queryObject = {
-        q: req.query.q,
-        diet: req.query.diet,
-        calories: req.query.calories,
-        health: req.query.health,
-        ingr: req.query.ingr
+        q: req.query.q
     }
     request({
-        url:urlMicroService,
+        url: urlMicroService,
         qs: queryObject
     }, function (error, response, body) {
         if (error) {
@@ -39,10 +119,5 @@ router.get('/food_search', function (req, res) {
         }
     })
 })
-
-
-
-
-
 
 module.exports = router;
