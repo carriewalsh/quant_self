979a71ddd6ab0aa6a59ba3a7a133428b3b2a741a
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-07-10 19:23:42 -0600

    Adds commented out apiKey requirements for future functionality

--- routes/api/v1/meals.js
+++ routes/api/v1/meals.js
@@ -5,13 +5,34 @@ const environment = process.env.NODE_ENV || "development";
 const configuration = require("../../../knexfile")[environment];
 const database = require("knex")(configuration);
 const Food = require('../../../models/food');
-const Meal = require('../../../models/meal');
+// const Meal = require('../../../models/meal');
 const pry = require('pryjs')
 var mealsController = require('../../../controllers/api/v1/meals_controller')
 
+const { Model } = require('objection');
+Model.knex(database)
 
 router.get('/', async (req,res) => {
-  mealsController.index(req,res)
+  // if (req.body.apiKey) {
+    // const users = await database.select('apiKey').from('users')
+    // const apis = []
+    // users.forEach(user => {
+      // apis.push(user['apiKey'])
+    // })
+    // if (apis.includes(req.body.apiKey)) {
+      mealsController.index(req,res)
+    // }
+    // else {
+      // res.status(401).json({
+        // error: "Unauthorized"
+      // })
+    // }
+  // }
+  // else {
+  //   res.status(401).json({
+  //     error: "Unauthorized"
+  //   })
+  // }
 })
 
 module.exports = router;
